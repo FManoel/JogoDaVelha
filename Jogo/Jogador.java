8f1d13e2bdceaca4e05de9173991776bd897d415
FManoel <f.manoel.p.m.adecarvalho@gmail.com>
2020-07-21 21:30:06 -0300

    first commit

---
+++ Jogo/Jogador.java
@@ -0,0 +1,18 @@
+package Jogo;
+
+public class Jogador 
+{
+	static String simbolo;
+	static int njogador;
+
+	static String jogador(int x)
+	{
+		simbolo = (x % 2 == 0) ? "X" : "O";
+		numeroJogador();
+		return simbolo;
+	}
+	static private void numeroJogador()
+	{
+		njogador = (simbolo.equals("X")) ? 1 : 2;
+	}
+}
---
+++ Jogo/Main.java
@@ -0,0 +1,11 @@
+package Jogo;
+
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{ 
+		Partida.partida();
+	}
+
+}
